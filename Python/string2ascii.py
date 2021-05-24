6a7fe7b77026a983a6765c380fe70980ee64cdb2
Mariano Muñoz Masís <65138325+MarianoIDC@users.noreply.github.com>
2021-05-23 23:09:40 -0600

    Update

--- Python/string2ascii.py
+++ Python/string2ascii.py
@@ -1,17 +1,4 @@
-from os import terminal_size
-
-
-def validateString(text, n):
-    for i in range(n):
-        number = ord(text[i].lower())
-        print(number)
-        if (96<number<122 or number==32 or number==241):
-            print("caracter valido en la posicion: "+str(i))
-        else:
-            print("caracter "+text[i]+" no valido en la posicion: "+str(i))
-            return False
-    return True
-            
+from validateStr import *
 
 def string2asc(text):
     text2ascii = []
